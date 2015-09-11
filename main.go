28a7c3e195a6efdc5983a4dce6bd172e896e011f
ZhiFeng Hu <hufeng1987@gmail.com>
2015-09-11 08:12:17 +0800

    Test command argument
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -7,6 +7,23 @@ import (
 
 func main() {
 	args := os.Args[1:]
+	argslen := len(args)
+	if argslen < 2 {
+		fmt.Println("rootsh update|check git|emacs")
+		os.Exit(0)
+	}
+	switch args[0] {
+	case "update":
+		go func(p string) {
+			fmt.Printf("update packages %s\n", p)
+		}(args[1])
+	case "check":
+		go func(p string) {
+			fmt.Printf("Check update %s\n", p)
+		}(args[1])
+	default:
+		fmt.Println("You must put some command")
+	}
 	fmt.Println(args)
 	fmt.Println("Hello world")
 }
